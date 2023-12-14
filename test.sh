859dd13e797693618a16fdeea04041e035d95129
Deulyne <destin.deulyne@gmail.com>
2023-12-14 23:37:10 +0100

    test du debuggage

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ce script demande le prénom de l'utilisateur et lui souhaite la bienvenue.
+
+# Demander le prénom à l'utilisateur
+echo "Bonjour ! Quel est votre prénom ?"
+read prenom
+
+# Afficher un message de bienvenue
+echo "Bienvenue, $prenom ! C'est un plaisir de vous rencontrer."
