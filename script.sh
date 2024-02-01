1e04c3120ea1d78024086b5322b051d093be52f5
Deulyne <destin.deulyne@gmail.com>
2024-02-01 17:51:24 +0100

    update script.sh translation done

--- script.sh
+++ script.sh
@@ -1,28 +1,44 @@
 #!/bin/bash
 
-# FONCTIONS
+###################################################################################################################################
 
-# Creation des dossiers temp et images
+# File Name: script.sh
+# Author: Deulyne DESTIN, Asma KAJEIOU, Emma DOS SANTOS
+# Created on: December ..., 2023
+# Description: (à mettre à jour)
+#   This program collects all the stages of the journeys made by different drivers.
+#   It will then sum the distances of the different steps while storing the min and max distances of the trips.
+#   These data will be sorted in descending order of "max distance - min distance",
+#   and then written to a csv output file as: 
+#   Route_ID;min_distance;max_distance;average_distance;(max_distance-min_distance)
+
+###################################################################################################################################
+
+
+###### FUNCTIONS #######
+
+
+# Creation of temp and image folders
 create_directories(){
     file="temp"
-    # Verifier si le dossier existe
+    # Check if the folder exists
     if [ -d "$file" ]
     then
-        # find : Cela garantit que tous les fichiers et sous-répertoires dans temp sont supprimés, même si le répertoire est déjà vide
+        # Ensures that all files and subdirectories in temp are deleted, even if the directory is already empty
         find temp -mindepth 1 -delete 
     fi
-    # Creer les dossiers
+
     mkdir -p temp images
 }
 
-# Verification de la presence de l'executable C
+# Verification of the presence of the C executable according to the treatments
 executable_verification(){
     case $1 in
         -t)
         if [ ! -f progc/progt ]
         then
             gcc -o progc/progt progc/programme_t.c
-            # Verifier si la compilation s'est bien deroulee
+            # Check if the compilation went well
             if [ $? -ne 0 ]
             then
                 echo "Erreur lors de la compilation. Veuillez corriger les erreurs avant de continuer."
@@ -32,7 +48,7 @@ executable_verification(){
         if [ ! -f progc/progt2 ]
         then
             gcc -o progc/progt2 progc/programme_t2.c
-            # Verifier si la compilation s'est bien deroulee
+            # Check if the compilation went well
             if [ $? -ne 0 ]
             then
                 echo "Erreur lors de la compilation. Veuillez corriger les erreurs avant de continuer."
@@ -43,9 +59,8 @@ executable_verification(){
         -s)
         if [ ! -f progc/progs ]
         then
-            #gcc -o progc/progs progc/programme_s.c
             gcc -o progc/progs progc/programme_s.c
-            # Verifier si la compilation s'est bien deroulee
+            # Check if the compilation went well
             if [ $? -ne 0 ]
             then
                 echo "Erreur lors de la compilation. Veuillez corriger les erreurs avant de continuer."
@@ -57,39 +72,41 @@ executable_verification(){
             echo "L'option $option n'est pas reconnue. Veuillez réessayer."
             exit 1 ;;
     esac
-# echo "L'executable C est present."
 }
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------
 
-# MAIN
+###### MAIN #######
 
-# Récupération du fichier CSV passé en argument
+
+# Recovery of the CSV file passed as argument
 input_file=$1
 
-# Vérification de l'existence du fichier
+
+# Checking the existence of the file
 if [ ! -f "$input_file" ]
 then
     echo "Le fichier $input_file n'existe pas."
     exit 1
 fi
-# Vérification de l'extension du fichier
+# Checking the file extension
 if [[ ! "$input_file" =~ \.csv$ ]]
 then
     echo "Le fichier $input_file n'est pas un fichier .csv. Veuillez réessayer svp..."
     exit 1
 fi
 
-# Création du dossier "data" s'il n'existe pas
-mkdir -p data
-# Copie du fichier CSV dans le dossier data
-cp "$input_file" data/ #
 
-# Cas du -h
-# Boucle pour parcourir les arguments
+# Creation of the "data" folder if it does not exist
+mkdir -p data
+# Copy the CSV file to the data folder
+cp "$input_file" data/ 
+
+
+# Treatment h
+# Loop that scans all arguments by checking the presence of the -t
 for arg in "$@"
 do
-    # Si l'argument est égal à "-h", alors on affiche l'aide
+    # If an argument is equal to "-h", then the help is displayed
     if [ "$arg" == "-h" ]
     then
     echo "---------------------------------------------------"
@@ -103,112 +120,94 @@ do
 
     exit 0
     fi
-
 done
 
-# Vérification des dossiers temp et images
+
+# Check temp and image directories
 create_directories
 
-# EXECUTION DES DIFFÉRENTS TRAITEMENTS
+# PROCESSING EXECUTION
 
-# Le premier argument est le fichier CSV
+# The first argument is the CSV file
 input_file=$1
+# The first argument is now the first argument given when running the script
 shift
 
 
-# Boucle pour traiter chaque argument 
+# Loop to process each argument 
 for option in "$@"
 do
    case $option in
         -d1)
             echo "Traitement D1..."
-            #cat "$input_file" >> temp/temp.csv
-            grep ";1;" "$input_file" > temp/firsttemp.csv
-            awk -F';' '{count[$6]+= 1} END {for (driver in count) print driver ";" count[driver]}' temp/firsttemp.csv >> temp/secondtemp.csv
 
-            # Trier la liste par ordre décroissant de nombre de trajets
-            sort -t';' -k2,2 -n -r temp/secondtemp.csv >> temp/thirdtemp.csv 
-            head -n 10 temp/thirdtemp.csv >> temp/finaltemp.csv
+            # Collects the first stages of each journey and count the number of tajets per driver
+            grep ";1;" "$input_file" > temp/firsttemp.csv
+            awk -F';' '{count[$6]+= 1} END {for (driver in count) print driver ";" count[driver]}' temp/firsttemp.csv > temp/secondtemp.csv
+
+            # Sorts the list in descending order of number of trips and get the first 10
+            sort -t';' -k2,2 -n -r temp/secondtemp.csv > temp/thirdtemp.csv 
+            head -n 10 temp/thirdtemp.csv > temp/finaltemp.csv
 
             echo "Les 10 conducteurs avec le plus de trajets sont : "
             cat temp/finaltemp.csv
 
-            # Calculez la durée totale d'exécution
-            execution_time=$(echo "$end_time - $start_time" | bc)
-
-            # Nettoyer les fichiers temporaires
+            # Clears temporary files
             rm temp/firsttemp.csv temp/secondtemp.csv temp/thirdtemp.csv temp/finaltemp.csv
 
             ;;
         -d2)
             echo "Traitement D2..."
-            #Recupérer 
-            #awk -F';' '{count[$6]+=$5} END {for (driver in count) print driver ";" count[driver]}' "$input_file" >> temp/firsttemp.csv
-            #awk -F';' '{count[$6]+=$5} END {for (driver in count) printf "%s;%.2f\n", driver, count[driver]}' "$input_file" >> temp/firsttemp.csv
-            LC_NUMERIC="en_US.UTF-8" awk -F';' '{count[$6] += $5} END {for (driver in count) printf "%s;%.6f\n", driver, count[driver]}' "$input_file" >> temp/firsttemp.csv
-            #LC_NUMERIC="en_US.UTF-8" awk -F';' '{count[$6] += $5} END {asorti(count, sorted_indices); for (i in sorted_indices) printf "%s;%.6f\n", sorted_indices[i], count[sorted_indices[i]]}' "$input_file" >> temp/firsttemp.csv
 
+            # Gets the total distance travelled by each driver
+            LC_NUMERIC="en_US.UTF-8" awk -F';' '{count[$6] += $5} END {for (driver in count) printf "%s;%.6f\n", driver, count[driver]}' "$input_file" > temp/firsttemp.csv
 
-
-            # Trier la liste par ordre décroissant des distances totales
-            sort -t';' -k2,2 -n -r temp/firsttemp.csv >> temp/secondtemp.csv 
-            
-            #longest_10_distances=$(head -n 10 temp/finaltemp.csv)
+            # Sorts the list in descending order according to total distances and gets the first 10
+            sort -t';' -k2,2 -n -r temp/firsttemp.csv > temp/secondtemp.csv 
             head -n 10 temp/secondtemp.csv >> temp/finaltemp.csv
-            
             echo "Les 10 conducteurs avec les plus grandes distances sont : "
             cat temp/finaltemp.csv
-            # Calculez la durée totale d'exécution
-            execution_time=$(echo "$end_time - $start_time" | bc)
-            
+
+            # Clears temporary files
             rm temp/firsttemp.csv temp/finaltemp.csv temp/secondtemp.csv
             ;;
         -l)
             echo "Traitement L..."
-            LC_NUMERIC="en_US.UTF-8" awk -F';' '{ sum[$1] += $5 } END { for (traject in sum) { formatted_value=sprintf("%.6f", sum[traject]); print traject ";" formatted_value } }' "$input_file" >> temp/templ.csv
 
-            # trier les plus longs trajets
-            sort -t ';' -k2,2 -n -r temp/templ.csv >> temp/tempcorrected.csv  
-           
-            # Récupérer les 10 premiers trajets
-            head -n 10 temp/tempcorrected.csv >> temp/tempfinal.csv
-           
-            #trier les 10 trajets par numéro d'identification croissant
-            sort -t ';' -k1,1 -n -r temp/tempfinal.csv >> temp/tempdone.csv
-            longest_10_trajects=$(head -n 10 temp/tempdone.csv)
+            # Recovers the total distance of each journey
+            LC_NUMERIC="en_US.UTF-8" awk -F';' '{ sum[$1] += $5 } END { for (traject in sum) { formatted_value=sprintf("%.6f", sum[traject]); print traject ";" formatted_value } }' "$input_file" > temp/firsttemp.csv
 
-            # Créer le graphique de type histogramme
+            # Sorts the list in descending order according to total distances and get the first 10
+            sort -t ';' -k2,2 -n -r temp/firsttemp.csv > temp/secondtemp.csv
+            head -n 10 temp/secondtemp.csv > temp/thirdtemp.csv 
+           
+            # Sorts trips by increasing identification number
+            sort -t ';' -k1,1 -n -r temp/thirdtemp.csv  > temp/finaltemp.csv
+
+            longest_10_trajects=$(head -n 10 temp/finaltemp.csv)
             echo "Les 10 trajets les plus longs sont : "
             echo "$longest_10_trajects"
-            # Calculez la durée totale d'exécution
-            execution_time=$(echo "$end_time - $start_time" | bc)
 
-            # Nettoyer les fichiers temporaires
-            rm temp/temp.csv temp/templ.csv temp/tempcorrected.csv temp/tempfinal.csv temp/tempdone.csv
+            # Clears temporary files
+            rm temp/firsttemp.csv temp/secondtemp.csv temp/thirdtemp.csv temp/finaltemp.csv
            
             ;;
         -t)
             echo "Traitement T..."
-            # Vérification de l'executable c
+
+            # Check the executable c
             executable_verification "$option"
-            #tail -n +2 "$input_file" > temp/firsttemp.csv
-            #awk -F';' '{print $3";"$4";"$1";"$2}' temp/firsttemp.csv >> temp/secondtemp.csv
-
-            # head -n 2000000 temp/temp.csv > temp/secondtemp.csv
-
-  #awk avec ville de depart
-            #awk -F';' '$2 == 1 {departure_city[$3]+=1 et count[$3]++ } END {for (city in departure_city) print city ";" departure_city[city]}' "$input_file" >> temp/firsttemp.csv
-
-             # Recupère le nombre de trajets qui parcourent chaque ville, ainsi que le nombre de fois où ces villes ont été des villes de départ de trajets.
-            #awk -F';' 'BEGIN { OFS=";"; } { count[$4] += 1; if ($2 == 1) { departure_city[$3] += 1; count[$3] += 1; } } END { for (city in count) print city, count[city] ";" (city in departure_city ? departure_city[city] : 0) }' "$input_file" >> temp/firsttemp.csv
-
+            
+            # Will count the number of times each city is crossed and the number of times they are departure city 
             awk -F';' 'BEGIN { OFS=";"; }
             { 
                 route_id = $1;
                 townA = $3;
                 townB = $4;
 
-                # Vérifier si townA ou townB ne sont pas dans route_id SUBSEP town dans visited_cities
+                # Check if Route_ID/townA or Route_ID/townB are not already present in the visited_cities table, 
+                # otherwise a new box of the table with these values is created and initialized to 1
+
                 if (!(route_id SUBSEP townA in visited_cities) || !(route_id SUBSEP townB in visited_cities)) {
                     if (!(route_id SUBSEP townA in visited_cities)) {
                         visited_cities[route_id SUBSEP townA] = 1;
@@ -220,29 +219,32 @@ do
                     }
                 }
 
+                # If the journey step is 1, then the value assigned to townA is incremented in the departure_city table
+
                 if ($2 == 1) { 
                     departure_city[$3] += 1;
                 }
             }
+            
+            # Displays the results in a temporary file
             END { 
                 for (city in count) {
                     print city, count[city] ";" (city in departure_city ? departure_city[city] : 0);
                 }
-            }' "$input_file" >> temp/firsttemp.csv
+            }' "$input_file" > temp/firsttemp.csv
 
+            # Sort by descending order the cities according to the number of times they are crossed and get the first 10
             ./progc/progt temp/firsttemp.csv
-             #sort -t ';' -k2,2 -n -r temp/firsttemp.csv >> temp/secondtemp.csv
+            head -n 11 temp/secondtemp.csv > temp/thirdtemp.csv
 
+            # Alphabetize the top 10 cities 
+            ./progc/progt2 temp/thirdtemp.csv
+            cat temp/finaltemp.csv
 
-             head -n 11 temp/secondtemp.csv >> temp/thirdtemp.csv
-             #awk 'NF{printf "%s", $0; getline; print}' temp/thirdtemp.csv > temp/ok.csv
-
-             ./progc/progt2 temp/thirdtemp.csv
-             #sort -t ';' -k2,1 -n temp/thirdtemp.csv >> temp/finaltemp.csv
-
-             cat temp/finaltemp.csv
-             rm temp/firsttemp.csv temp/thirdtemp.csv temp/secondtemp.csv temp/finaltemp.csv
+            # Clears temporary files
+            rm temp/firsttemp.csv temp/thirdtemp.csv temp/secondtemp.csv temp/finaltemp.csv
            ;;
+
         -s)
            echo "Traitement S..."
             # verification of the c executable
@@ -261,11 +263,12 @@ do
             echo "Les 50 premiers sont : "
             cat temp/finaltemp.csv
             
+            # Clears temporary files
             rm temp/output.csv temp/firsttemp.csv temp/secondtemp.csv temp/finaltemp.csv
             ;;
 
         *)
-            echo "L'option $option n'est pas reconnue. Veuillez réessayer."
+            echo "The option $option is not recognized... Please try again.\n"
             exit 1 ;;
     esac
 done
